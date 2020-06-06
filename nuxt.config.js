f9c10af884bc14bedec3148565268d67a28d058f
Eduardo Barros <joseeduardobarros@gmail.com>
2020-06-06 17:40:48 -0300

    Change meta description property and fix bug

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ const routerBase =
   process.env.DEPLOY_ENV === 'GH_PAGES'
     ? {
         router: {
-          base: '/<repository-name>/'
+          base: '/resume/'
         }
       }
     : {}
@@ -22,7 +22,7 @@ export default {
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content: 'Eduardo Barros - Resume'
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
