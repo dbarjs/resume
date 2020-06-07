e77aa3df7090bb66d7d4099ecd0895e31e1faf89
Eduardo Barros <joseeduardobarros@gmail.com>
2020-06-07 17:42:18 -0300

    Create AboutSection component

---
+++ components/AboutSection.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="mb-6">
+    <h1 class="mb-6">
+      Eduardo Barros
+      <span class="print-hidden">
+        Engenheiro de Software
+      </span>
+    </h1>
+    <h2 class="print-hidden">Sobre</h2>
+    <p>
+      Sou um Geek apaixonado por Tecnologia e Inovação. Além de atuar há 6 anos
+      como Desenvolvedor Full Stack em aplicações Web e como Consultor de
+      Tecnologia, também sou estudante de Gestão Empresarial e Marketing.
+    </p>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
