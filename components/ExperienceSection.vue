19f7c8d6e05474deaef2f7bb8f76cc8900c9c4d8
Eduardo Barros <joseeduardobarros@gmail.com>
2020-06-08 17:42:07 -0300

    Create ExperienceSection component

---
+++ components/ExperienceSection.vue
@@ -0,0 +1,221 @@
+<template>
+  <section>
+    <h2>Experiência</h2>
+    <article class="job">
+      <h3 class="job-employer">JSBX Consulting</h3>
+      <p class="job-title">Fundador e Engenheiro de Software</p>
+      <p class="job-time">Fevereiro de 2020 - o momento (4 meses)</p>
+      <p class="job-city">Bandeirantes, Paraná</p>
+      <p class="job-description">
+        Criei a consultoria JSBX com objetivo de adquirir aprendizado com
+        Tecnologias em tempo real ao ofercer soluções corporativas desenvolvidas
+        em plataforma Web.
+      </p>
+      <ul class="job-technologies">
+        <li>Vue.js</li>
+        <li>Webpack</li>
+        <li>AWS DynamoDB</li>
+        <li>GCP</li>
+        <li>Google Firestore</li>
+        <li>Nuxt.js</li>
+      </ul>
+    </article>
+    <article class="job">
+      <h3 class="job-employer">Elbit Digital</h3>
+      <p class="job-title">Co-Fundador</p>
+      <p class="job-time">Jun. de 2016 - o momento (4 anos)</p>
+      <p class="job-city">Palmital, São Paulo</p>
+      <p class="job-description">
+        Fundei a Elbit Digital junto de uma equipe apaixonada por Web com apoio
+        da Kabana's Info. Como líder do time de desenvolvimento Web pude
+        garantir a entrega de muitos Websites de alta qualidade com foco em
+        premiações de Web Design como Awwwards e FDA. Na Elbit, foi essencial
+        ter pensamento criativo e treinar pessoas.
+      </p>
+      <ul class="job-technologies">
+        <li>JavaScript</li>
+        <li>PostCSS</li>
+        <li>Gulp.js</li>
+        <li>Google Firebase</li>
+        <li>MySQL</li>
+      </ul>
+    </article>
+    <article class="job">
+      <h3 class="job-employer">JXB Agro</h3>
+      <p class="job-title">Engenheiro de Software</p>
+      <p class="job-time">Abr. de 2019 - Fev. de 2020 (11 meses)</p>
+      <p class="job-city">Itambaracá, Paraná</p>
+      <p class="job-description">
+        A JXB Agro é uma empresa de produção agrícola e fui responsável por
+        desenvolver soluções Web para aprimorar a gestão dos recursos e controle
+        dos custos de produção, além de exibir dados úteis para planejamento,
+        como monitoramento de clima e produção.
+      </p>
+      <ul class="job-technologies">
+        <li>Vue.js</li>
+        <li>Vuetify</li>
+        <li>Google Cloud Plataform</li>
+        <li>Axios</li>
+        <li>Express</li>
+        <li>MongoDB</li>
+      </ul>
+    </article>
+    <article class="job">
+      <h3 class="job-employer">Du Lago Refeições</h3>
+      <p class="job-title">Engenheiro de Software</p>
+      <p class="job-time">Dez. de 2017 - Abr. de 2019 (1 ano e 5 meses)</p>
+      <p class="job-city">Bandeirantes, Paraná</p>
+      <p class="job-description">
+        A partir do modelo de negócio desenvolvido como Gestor, liderei o
+        desenvolvimento de uma aplicação Web para ser comercializada para
+        serviços de delivery. O principal desafio foi desenvolver um framework
+        Web de persistencia de dados em rede local sem servidor ativo utilizando
+        WebRTC, IndexedDB e Firestore.
+        <!-- Utilizei tecnologias como IndexedDB, WebRTC e
+        Firestore para criar um sistema de persistência de dados em rede local
+        sem a necessidade de um servidor ativo. -->
+      </p>
+      <ul class="job-technologies">
+        <li>Vue.js</li>
+        <li>Webpack</li>
+        <li>Express</li>
+        <li>PostgreSQL</li>
+        <li>Google Firestore</li>
+      </ul>
+      <br />
+      <p class="job-title">Gerente Administrativo</p>
+      <p class="job-time">Nov. de 2016 - Dez. de 2017 (1 ano e 1 mês)</p>
+      <p class="job-city">Bandeirantes, Paraná</p>
+      <p class="job-description">
+        Na Du Lago Refeições, assumi a responsabilidade de criar, implantar e
+        garantir a qualidade de processos a partir de soluções inovadoras e
+        eficientes nas áreas de produção, vendas, marketing, TI, planejamento,
+        compras e logística. Me tornei um lider muito mais humano, aprimorei
+        minha habilidade organizacional e evolui com trabalho em equipe.
+      </p>
+    </article>
+    <article class="job">
+      <h3 class="job-employer">Kabana's Info</h3>
+      <p class="job-title">Engenheiro de Software</p>
+      <p class="job-time">Mar. de 2015 - Nov. de 2016 (1 ano e 9 meses)</p>
+      <p class="job-city">Palmital, São Paulo</p>
+      <p class="job-description">
+        Trabalhando com desenvolvimento Web na Kabana's Info, pude evoluir muito
+        com UI/UX, JavaScript, automatização de tarefas com Node.js e construção
+        APIs REST em PHP com PostgreSQL. Criei o Framework Villa.js para
+        prototipagem de aplicações Web. Desenvolvi muita empatia ao treinar
+        iniciantes em Web.
+      </p>
+      <ul class="job-technologies">
+        <li>JavaScript</li>
+        <li>PHP</li>
+        <li>Google Firebase</li>
+      </ul>
+      <br />
+      <p class="job-title">Analista Junior</p>
+      <p class="job-time">Jan. de 2014 - Mar. de 2015 (1 ano e 2 meses)</p>
+      <p class="job-city">Palmital, São Paulo</p>
+      <p class="job-description">
+        Iniciei minha carreira como desenvolvedor de software ao ser convidado
+        para trabalhar junto do pessoal da Kabana's Info, uma empresa fantástica
+        com muitos talentos que me incentivaram a trabalhar em inúmeros projetos
+        desafiadores e que pude imergir em diversas tecnologias. Acompanhar
+        visitas a clientes me possibilitaram aprender muito sobre vendas e
+        relacionamento com clientes.
+      </p>
+      <ul class="job-technologies">
+        <li>PHP</li>
+        <li>JavaScript</li>
+        <li>CSS3</li>
+        <li>HTML5</li>
+        <li>Java</li>
+        <li>Primefaces</li>
+        <li>IOT</li>
+        <li>PostgreSQL</li>
+        <li>Bootstrap</li>
+      </ul>
+    </article>
+    <article class="job">
+      <h3 class="job-employer">Mister Rango</h3>
+      <p class="job-title">Engenheiro de Software</p>
+      <p class="job-time">Set. de 2014 - Mar. de 2015 (7 meses)</p>
+      <p class="job-city">Assis, São Paulo</p>
+      <p class="job-description">
+        Em parceria com a Kabana's Info, desenvolvemos uma Dashboard completa
+        utilizando Qlikview no escritório central Mister Rango para visualizar
+        relatórios em tempo real de sua rede de restaurantes. Para concluir este
+        desafio, evolui muito com ciência de dados, gestão empresarial e
+        colaboração em equipe.
+      </p>
+      <ul class="job-technologies">
+        <li>QlikView</li>
+        <li>SQL Server</li>
+        <li>Office Excel</li>
+        <li>Visual Basic</li>
+        <li>Windows Server</li>
+      </ul>
+    </article>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.job {
+  @apply mb-8;
+}
+
+.job-employer,
+.job-title,
+.job-description {
+  @apply text-black;
+}
+
+.job-city,
+.job-description,
+.job-time {
+  @apply text-sm;
+}
+
+.job-city {
+  @apply text-gray-500;
+}
+
+.job-description {
+  @apply mt-3;
+}
+
+.job-technologies {
+  @apply flex flex-wrap text-xs;
+}
+
+.job-technologies:before {
+  content: 'Conhecimentos adquiridos:';
+  margin-right: 0.25rem;
+}
+
+.job-technologies li:after {
+  content: ',';
+  margin-right: 0.25rem;
+}
+
+.job-technologies li:last-child:after {
+  content: '.';
+}
+
+@media print {
+  .job {
+    @apply mb-6;
+  }
+
+  .job-description {
+    @apply text-xs mt-0 text-gray-900;
+  }
+
+  .job-technologies {
+    @apply hidden;
+  }
+}
+</style>
